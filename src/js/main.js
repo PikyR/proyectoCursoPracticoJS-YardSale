9a22d4f3a46df43ab789bafbf631edcead8249e0
pikyr <pikytdv@gmail.com>
2022-08-16 21:56:54 -0300

    Fin de clase 18 - Fusión del menu en desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-// alert("Hello wero");
+const headerEmail = document.querySelector('.navbar__email');
+const menuDesktop = document.querySelector('.menu-desktop')
+
+headerEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+  menuDesktop.classList.toggle('inactive');
+}
