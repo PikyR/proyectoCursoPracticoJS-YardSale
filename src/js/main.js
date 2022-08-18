3014e347210a4ef8bf42b941b258a25b419a77fb
pikyr <pikytdv@gmail.com>
2022-08-18 10:35:50 -0300

    Fin de clase 19 - Fusion del menú en mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 const headerEmail = document.querySelector('.navbar__email');
-const menuDesktop = document.querySelector('.menu-desktop')
+const menuDesktop = document.querySelector('.menu-desktop');
+const burguerMenuIcon = document.querySelector('.navbar__burger-menu');
+const menuMobile = document.querySelector('.menu-mobile-container');
+
 
 headerEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
   menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  menuMobile.classList.toggle('inactive');  
 }
