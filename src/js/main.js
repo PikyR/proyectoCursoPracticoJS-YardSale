de2983a1dea9a82169c3aded52c90d45a4e86042
pikyr <pikytdv@gmail.com>
2022-08-19 22:56:12 -0300

    Fin de clase 21 - HTML a partir de arrays

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const burguerMenuIcon = document.querySelector('.navbar__burger-menu');
 const menuMobile = document.querySelector('.menu-mobile-container');
 const cartIcon = document.querySelector('.navbar__cart');
 const shoppingCart = document.querySelector('.shopping-cart');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 headerEmail.addEventListener('click', toggleDesktopMenu);
@@ -65,7 +66,7 @@ productsList.push({
   image: "assets/images/pineapple.jpg"
 })
 productsList.push({
-  name: "lighter",
+  name: "Lighter",
   price: 456,
   image: "assets/images/lighter.jpg"
 })
@@ -75,32 +76,41 @@ productsList.push({
   image: "assets/images/camera.jpg"
 })
 
-for (const product of productsList) {
-  const productCard = document.createElement('article');
-  productCard.classList.add('product-card');
-
-  const productCardImg = document.createElement('img');
-  productCardImg.classList.add('product-card__img');
-  productCardImg.setAttribute('src', product.image);
-
-  const productCardDetail = document.createElement('div');
-  productCardDetail.classList.add('product-card__detail');
+function renderProducts(list) {
+  for (const product of list) {
+    const productCard = document.createElement('article');
+    productCard.classList.add('product-card');
+    
+    const productCardImg = document.createElement('img');
+    productCardImg.classList.add('product-card__img');
+    productCardImg.setAttribute('src', product.image);
   
-  const productCardInfo = document.createElement('div');
-  productCardInfo.classList.add('product-card__info');
+    const productCardDetail = document.createElement('div');
+    productCardDetail.classList.add('product-card__detail');
   
-  const productCardPrice = document.createElement('p');
-  productCardPrice.classList.add('product-card__price');
-  productCardPrice.innerText = `$ ${product.price}`;
+    const productCardInfo = document.createElement('div');
+    productCardInfo.classList.add('product-card__info');
   
-  const productCardName = document.createElement('p');
-  productCardName.classList.add('product-card__product-name');
-  productCardName.innerText = product.name;
-
-  const productCardCartIcon = document.createElement('figure');
-  productCardCartIcon.classList.add('product-card__icon');
+    const productCardPrice = document.createElement('p');
+    productCardPrice.classList.add('product-card__price');
+    productCardPrice.innerText = `$ ${product.price}`;
+    
+    const productCardName = document.createElement('p');
+    productCardName.classList.add('product-card__product-name');
+    productCardName.innerText = product.name;
   
-  const productCardCartIconImg = document.createElement('img');
-  productCardCartIconImg.classList.add('product-card__icon');
-  productCardCartIconImg.setAttribute('src', 'assets/icons/bt_add_to_cart.svg');
+    const productCardCartIcon = document.createElement('figure');
+    productCardCartIcon.classList.add('product-card__icon');
+  
+    const productCardCartIconImg = document.createElement('img');
+    productCardCartIconImg.classList.add('product-card__icon');
+    productCardCartIconImg.setAttribute('src', 'assets/icons/bt_add_to_cart.svg');
+  
+    productCardCartIcon.appendChild(productCardCartIconImg);
+    productCardInfo.append(productCardPrice, productCardName);
+    productCardDetail.append(productCardInfo, productCardCartIcon);
+    productCard.append(productCardImg,productCardDetail);
+    
+    cardsContainer.append(productCard);
+  }
 }
